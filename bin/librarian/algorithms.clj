4eb9f8b465b23931ec0c0305c02bd04bbd3a09d7
CarpeDiem CarpeNoctem <carpenoctemengineer@gmail.com>
2016-10-11 11:27:11 +0200

    Create euclid function

--- bin/librarian/algorithms.clj
+++ bin/librarian/algorithms.clj
@@ -2,3 +2,9 @@
 
 (defn si [p1 p2] (filter p1 (keys p2)))
 
+(defn euclid
+  [p1 p2]
+  (if (empty? (si p1 p2))
+    (int 0)
+    (let [squares-sum (float (reduce #(+ %1 (float (Math/pow (- (int (p1 %2)) (int (p2 %2))) (int 2)))) (int 0) (si p1 p2)))]
+      (/ (int 1) (+ squares-sum (int 1))))))
