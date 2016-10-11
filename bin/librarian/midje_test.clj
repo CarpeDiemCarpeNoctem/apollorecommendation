3842c246b22b1d880639853fd50594cc2cf60b6a
CarpeDiem CarpeNoctem <carpenoctemengineer@gmail.com>
2016-10-11 18:40:26 +0200

    Add midje test for euclidean function when items don't perfectly correlate

--- bin/librarian/midje_test.clj
+++ bin/librarian/midje_test.clj
@@ -4,4 +4,8 @@
 
 (facts "ns:algorithms 'euclid'"
   (fact "Returns Euclidean distance score"
-    (euclid {:123 3 :234 5 :345 4} {:123 3 :234 5 :345 4}) => 1.0))
+    (euclid {:123 3 :234 5 :345 4} {:123 3 :234 5 :345 4}) => 1.0)
+  
+  (fact "Returns Euclidean distance score"
+    (euclid {:123 3 :234 5 :345 4} {:123 4 :345 5 :456 2 :567 3.0, :678 5.0}) => 0.3333333333333333))
+
