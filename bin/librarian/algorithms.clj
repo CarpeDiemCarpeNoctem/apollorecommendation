2b1e3a8124c4eeeb578357cba37ff55437bc01fe
CarpeDiem CarpeNoctem <carpenoctemengineer@gmail.com>
2016-10-11 19:03:40 +0200

    Add pearson function

--- bin/librarian/algorithms.clj
+++ bin/librarian/algorithms.clj
@@ -7,3 +7,24 @@
       0
       (let [squares-sum (reduce #(+ %1 (Math/pow (- (p1 %2) (p2 %2)) 2)) 0 si)]
         (/ 1 (+ squares-sum 1))))))
+
+(defn pearson
+  [user friend]
+  (let [books-both-read (filter user (keys friend))] 
+    (if (empty? books-both-read)
+      0
+      (let [number-of-books (count books-both-read)
+            ; sums
+            sum-user-ratings (reduce #(+ %1 (user %2)) 0 books-both-read)
+            sum-friend-ratings (reduce #(+ %1 (friend %2)) 0 books-both-read)
+            ; sum of squared values
+            sum-squared-user-ratings (reduce #(+ %1 (Math/pow (user %2) 2)) 0 books-both-read)
+            sum-squared-friend-ratings (reduce #(+ %1 (Math/pow (friend %2) 2)) 0 books-both-read)
+            ; sum of their products
+            sum-products (reduce #(+ %1 (* (user %2) (friend %2))) 0 books-both-read)
+            covariance (- sum-products (/ (* sum-user-ratings sum-friend-ratings) number-of-books))
+            standard-deviation (Math/sqrt (* (- sum-squared-user-ratings (/ (Math/pow sum-user-ratings 2) number-of-books))
+                                             (- sum-squared-friend-ratings (/ (Math/pow sum-friend-ratings 2) number-of-books))))]
+        (if (= standard-deviation 0.0)
+          0
+          (/ covariance standard-deviation))))))
