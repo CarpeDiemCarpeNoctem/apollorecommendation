4640a615a4a665b0313bdf0f0188a10168dbb278
CarpeDiem CarpeNoctem <carpenoctemengineer@gmail.com>
2016-10-11 08:36:43 +0200

    Add page title

--- src/librarian/views.clj
+++ src/librarian/views.clj
@@ -1,9 +1,16 @@
 (ns librarian.views
   (:require [hiccup.page :as hic-p]))
 
+(defn gen-page-head
+  []
+  [:head
+   [:title (str "My Librarian")]
+   ])
+
 (defn home-page
   []
   (hic-p/html5
+   (gen-page-head)
    [:h1 {:id "maintitle"} "My Librarian"]
    [:div {:id "intro"}
     "Welcome to My Librarian book recommendation service"]))
