80917381adfe5b3e8fa4a24cfa3fa8b61831ff68
CarpeDiem CarpeNoctem <carpenoctemengineer@gmail.com>
2016-10-18 01:33:21 +0200

    Add midje test for jaccard-index function when one of the supplied collections is empty

--- bin/librarian/midje_test.clj
+++ bin/librarian/midje_test.clj
@@ -74,7 +74,10 @@
     (jaccard-index {:123 3 :234 5 :345 4} {:123 3 :234 5 :345 4}) => 1)
   
   (fact "Returns Jaccard index score"
-    (jaccard-index {:123 3 :234 5 :345 4} {:123 4 :345 5 :456 2 :567 3.0, :678 5.0}) => 1/3))
+    (jaccard-index {:123 3 :234 5 :345 4} {:123 4 :345 5 :456 2 :567 3.0, :678 5.0}) => 1/3)
+  
+  (fact "Returns Jaccard index score"
+    (jaccard-index {} {:123 4 :345 5 :456 2 :567 3.0, :678 5.0}) => 0))
 
 (facts "ns:ratings 'parse-xml'"
   (fact "Returns a map from supplied XML"
