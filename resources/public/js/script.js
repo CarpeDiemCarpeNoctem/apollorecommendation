9aebef4e2046b30511d6d1588cd47d1eebd88b70
CarpeDiem CarpeNoctem <carpenoctemengineer@gmail.com>
2016-10-19 00:21:38 +0200

    Add spinner gif image to images in resources and create a function in js script to load that image instead of text

--- resources/public/js/script.js
+++ resources/public/js/script.js
@@ -1,9 +1,18 @@
 
-document.getElementById("recommendbutton").addEventListener("click", loadingText);
+document.getElementById("recommendbutton").addEventListener("click", loadingImage);
 
 function loadingText() {
     if(document.getElementById("searchfield").checkValidity()) {
       document.getElementById("load").innerHTML = "Loading...";
+    }
+    else{
+      document.getElementById("load").innerHTML = "Please check your input";
+    }
+}
+
+function loadingImage() {
+    if(document.getElementById("searchfield").checkValidity()) {
+      document.getElementById("load").innerHTML = "<img src=\'images/spinner.gif\'>";
     }
     else{
       document.getElementById("load").innerHTML = "Please check your input";
--- resources/public/js/script.js~
+++ resources/public/js/script.js~
@@ -2,5 +2,10 @@
 document.getElementById("recommendbutton").addEventListener("click", loadingText);
 
 function loadingText() {
+    if(document.getElementById("searchfield").checkValidity()) {
       document.getElementById("load").innerHTML = "Loading...";
+    }
+    else{
+      document.getElementById("load").innerHTML = "Please check your input";
+    }
 }
