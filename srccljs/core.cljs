20708a8963272583847bde70791d6e971cdcd63a
CarpeDiem CarpeNoctem <carpenoctemengineer@gmail.com>
2016-10-19 02:02:26 +0200

    Add validation check and change name of loader function to validation-loader in clojurescript

--- srccljs/core.cljs
+++ srccljs/core.cljs
@@ -2,13 +2,16 @@
   (:require [goog.events :as events]
             [goog.dom :as dom]))
 
-(defn loader
+(defn validation-loader
   []
   (let [image (atom "<img src='images/spinner.gif'>")
+        warning (atom "Please check your input")
         button  (dom/getElement "recommendbutton")
         display (dom/getElement "load")]
     (events/listen button "click"
                    (fn [event]
-                       (set! (.-innerHTML display) @image)))))
+                     (if (.checkValidity (dom/getElement "searchfield"))
+                       (set! (.-innerHTML display) @image)
+                       (set! (.-innerHTML display) @warning))))))
 
-(loader)
+(validation-loader)
