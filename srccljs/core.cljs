034397b661c5ce6051c67f2dd068e3a46e993733
CarpeDiem CarpeNoctem <carpenoctemengineer@gmail.com>
2016-10-19 02:15:23 +0200

    Add docstring to validation-loader function in clojurescript and comment explaining the role of the function

--- srccljs/core.cljs
+++ srccljs/core.cljs
@@ -2,7 +2,15 @@
   (:require [goog.events :as events]
             [goog.dom :as dom]))
 
+; - - - - - - - - - -
+; The core namespace contains clojurescript functions
+; that compile to javascript to help with user interaction
+; on the front-end of the book recommendation application
+; - - - - - - - - - -
+
 (defn validation-loader
+  "Checks if the input field on the front-end has valid input and loads a spinner image to show work
+   in progress. Otherwise it shows a warning message if the input is not valid."
   []
   (let [image (atom "<img src='images/spinner.gif'>")
         warning (atom "Please check your input")
