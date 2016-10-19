a2cfc7fa695afd6963d49ec5f42e26608c585707
CarpeDiem CarpeNoctem <carpenoctemengineer@gmail.com>
2016-10-19 04:07:08 +0200

    Add functions to unbox primitives in the pearson function

--- bin/librarian/formulas.clj
+++ bin/librarian/formulas.clj
@@ -15,22 +15,22 @@
   [user friend]
   (let [books-both-read (filter user (keys friend))] 
     (if (empty? books-both-read)
-      0
-      (let [number-of-books (count books-both-read)
+      (int 0)
+      (let [number-of-books (int (count books-both-read))
             ; sums
-            sum-user-ratings (reduce #(+ %1 (user %2)) 0 books-both-read)
-            sum-friend-ratings (reduce #(+ %1 (friend %2)) 0 books-both-read)
+            sum-user-ratings (reduce #(+ %1 (int (user %2))) (int 0) books-both-read)
+            sum-friend-ratings (reduce #(+ %1 (int (friend %2))) (int 0) books-both-read)
             ; sum of squared values
-            sum-squared-user-ratings (reduce #(+ %1 (Math/pow (user %2) 2)) 0 books-both-read)
-            sum-squared-friend-ratings (reduce #(+ %1 (Math/pow (friend %2) 2)) 0 books-both-read)
+            sum-squared-user-ratings (reduce #(+ %1 (Math/pow (int (user %2)) (int 2))) (int 0) books-both-read)
+            sum-squared-friend-ratings (reduce #(+ %1 (Math/pow (int (friend %2)) (int 2))) (int 0) books-both-read)
             ; sum of their products
-            sum-products (reduce #(+ %1 (* (user %2) (friend %2))) 0 books-both-read)
-            covariance (- sum-products (/ (* sum-user-ratings sum-friend-ratings) number-of-books))
-            standard-deviation (Math/sqrt (* (- sum-squared-user-ratings (/ (Math/pow sum-user-ratings 2) number-of-books))
-                                             (- sum-squared-friend-ratings (/ (Math/pow sum-friend-ratings 2) number-of-books))))]
+            sum-products (reduce #(+ %1 (* (int (user %2)) (int (friend %2)))) (int 0) books-both-read)
+            covariance (float (- sum-products (/ (* sum-user-ratings sum-friend-ratings) number-of-books)))
+            standard-deviation (float (Math/sqrt (* (- sum-squared-user-ratings (/ (Math/pow sum-user-ratings (int 2)) number-of-books))
+                                                    (- sum-squared-friend-ratings (/ (Math/pow sum-friend-ratings (int 2)) number-of-books)))))]
         (if (= standard-deviation 0.0)
-          0
-          (/ covariance standard-deviation))))))
+          (int 0)
+          (float (/ covariance standard-deviation)))))))
 
 (defn manhattan-distance
   [user friend]
