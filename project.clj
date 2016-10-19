95bdee9a6cfcd97894142c9cf804c0f82a7f806c
CarpeDiem CarpeNoctem <carpenoctemengineer@gmail.com>
2016-10-19 04:23:42 +0200

    Change description of application in project

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject librarian "0.1.0-SNAPSHOT"
-  :description "Book recommendation application"
+  :description "Apollo book recommendation service based on the Goodreads API"
   :url "TBA"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
